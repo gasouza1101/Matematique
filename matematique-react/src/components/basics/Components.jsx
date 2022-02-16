41671760a947037434ac75004ab56c5282f5b349
Gabriel Augusto Souza Silva - Stefanini <gabriel.silva.stefanini@elo.com.br>
2022-02-15 21:49:27 -0300

    First Commit

---
+++ matematique-react/src/components/basics/Components.jsx
@@ -0,0 +1,9 @@
+export default function(){
+    return <div>
+        <h1>eai jovem
+</h1>
+        <h2>eai jovem
+</h2>
+                
+        </div>
+}
---
+++ matematique-react/src/components/basics/First.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function First(){
+    const msg = "Sejam Bem Vindos(as)"
+    return <div><h2>Olá primeiro componente</h2>
+    { msg }
+    </div>
+}
